31c9f45fcc721543284a660294ed6ef512fd6970
Maz Jaleel <mazjaleel@gmail.com>
2019-05-28 23:59:49 +0400

    Use snake_case instead of camelCase for test names

--- Cucumberish/Utils/NSString+Formatter.m
+++ Cucumberish/Utils/NSString+Formatter.m
@@ -28,43 +28,16 @@
 @implementation NSString (Formatter)
 - (NSString *)camleCaseStringWithFirstUppercaseCharacter:(BOOL)firstUppercaseCharacter
 {
-    static NSString * sep = @"AASSAAAKKKAALLLAL";
-    NSString * str = [[self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] stringByReplacingOccurrencesOfString:@" " withString:sep];
-    str = [str stringByReplacingCharactersInSet:[[NSCharacterSet alphanumericCharacterSet] invertedSet] withString:@""];
-    str = [str stringByReplacingOccurrencesOfString:sep withString:@"_"];
-    NSMutableString * output = [NSMutableString string];
-    BOOL makeNextCharacterInUpperCase = firstUppercaseCharacter;
-    for (int i = 0; i < str.length; i++) {
-        
-        NSString * substr = [str substringWithRange:NSMakeRange(i, 1)];
-        if(i == 0 && !makeNextCharacterInUpperCase){
-            substr = [substr lowercaseString];
-        }
-        if([substr isEqualToString:@"_"]){
-            makeNextCharacterInUpperCase = YES;
-            continue;
-        }else if(makeNextCharacterInUpperCase){
-            substr = [substr uppercaseString];
-            
-        }
-        [output appendString:substr];
-        makeNextCharacterInUpperCase = NO;
-    }
-    
-    return output;
-}
+    NSString *trimmedString = [self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
 
-- (NSString *)stringByReplacingCharactersInSet:(NSCharacterSet *)charSet withString:(NSString *)aString {
-    NSMutableString *s = [NSMutableString stringWithCapacity:self.length];
-    for (NSUInteger i = 0; i < self.length; ++i) {
-        unichar c = [self characterAtIndex:i];
-        if (![charSet characterIsMember:c]) {
-            [s appendFormat:@"%C", c];
-        } else {
-            [s appendString:aString];
-        }
-    }
-    return s;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"\\s+"
+                                                                           options:NSRegularExpressionCaseInsensitive
+                                                                             error:nil];
+
+    return [regex stringByReplacingMatchesInString:trimmedString
+                                           options:0
+                                             range:NSMakeRange(0, [self length])
+                                      withTemplate:@"_"];
 }
 
 @end
